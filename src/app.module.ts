03bea06a5c879d68608ef0b31959d5c53eab68c7
Tony Santana Lopez <tonysantana1492@gmail.com>
2022-09-19 10:56:38 -0400

    configurate ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, //Como esta definido como global este modulo puede ser accedido sin necesidad de importarlo en otro modulo.
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),        
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        EXPIRES_IN: Joi.string().required()
+      }),
+    }),
   ],
   controllers: [],
   providers: [],
