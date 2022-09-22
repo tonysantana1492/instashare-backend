69ff2e4fed15c780e4eaebddbd4331cfdee57b70
Tony Santana Lopez <tonysantana1492@gmail.com>
2022-09-22 00:25:07 -0400

    fix error config

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,30 +14,26 @@ import { FilemanagerModule } from './filemanager/filemanager.module';
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
       ignoreEnvFile: process.env.NODE_ENV === 'production',
       validationSchema: Joi.object({
-        NODE_ENV: Joi.string().valid('dev', 'production').required(),        
+        NODE_ENV: Joi.string().valid('dev', 'production').required(),
         DB_HOST: Joi.string().required(),
         DB_PORT: Joi.number().required(),
         DB_USERNAME: Joi.string().required(),
         DB_PASSWORD: Joi.string().required(),
         DB_NAME: Joi.string().required(),
         JWT_SECRET: Joi.string().required(),
-        EXPIRES_IN: Joi.number().required()
+        EXPIRES_IN: Joi.number().required(),
       }),
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      ...(process.env.DATABASE_URL
-        ? { url: process.env.DATABASE_URL }
-        : {
-            host: process.env.DB_HOST,
-            port: parseInt(process.env.DB_PORT),
-            username: process.env.DB_USERNAME,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
-          }),
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       synchronize: process.env.NODE_ENV !== 'production',
       // logging: process.env.NODE_ENV !== 'prod' && process.env.NODE_ENV !== 'test',
-        entities: [join(__dirname,'**', '*.entity.{ts,js}')],
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
     }),
     EventEmitterModule.forRoot(),
     AuthModule,
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
-  const app = await NestFactory.create(AppModule);
-  
+  const app = await NestFactory.create(AppModule);  
   app.enableCors();
   const port = process.env.PORT || 4000;
   await app.listen(port);
