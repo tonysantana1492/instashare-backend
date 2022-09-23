5a015f44f7f7a38d4f76cb83b951ca93c6663016
Tony Santana Lopez <tonysantana1492@gmail.com>
2022-09-23 18:55:40 -0400

    Fix problem with CORS

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
-  const app = await NestFactory.create(AppModule);  
+  const app = await NestFactory.create(AppModule, { cors: true });  
   app.enableCors({
     origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
