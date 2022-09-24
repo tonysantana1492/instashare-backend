ef797b26446801ebff7f7513ce8a5fd9aba790d6
Tony Santana Lopez <tonysantana1492@gmail.com>
2022-09-23 23:30:16 -0400

    Config CORS

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);  
-  if (process.env.NODE_ENV === 'dev') {
+  if (process.env.NODE_ENV === 'production') {
+    app.enableCors({
+      origin: 'https://nest-instashare-backend.herokuapp.com',
+      credentials: true,
+    });
+  } else{
     app.enableCors();
-  } 
+  }
   const port = process.env.PORT || 4000;
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
